134bea526de070e292026596b7bbe16537052c17
anyemaciasr <amaciasr@unicesar.edu.co>
2022-11-16 18:16:23 -0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestburnoutComponent } from './testburnout/testburnout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestburnoutComponent
   ],
   imports: [
     BrowserModule,
