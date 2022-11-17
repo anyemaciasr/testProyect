e1f512e577879075cccdbeddcf647c88a20fedf8
anyemaciasr <amaciasr@unicesar.edu.co>
2022-11-17 18:30:17 -0500

    Aportes al sistema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Aportes } from './model/aportes';
 import { Test } from './model/test';
 
 @Component({
@@ -10,8 +11,10 @@ export class AppComponent  {
   title = 'testProyect';
  
   test: Test = new Test();
+  aportes:Aportes=new Aportes();
+  SMV:number=1000000;
   suma:Number=0;
-
+/*
   calcular(){
     this.sumaTotal();
     console.log(this.test.Total);
@@ -89,5 +92,71 @@ export class AppComponent  {
       this.test.ResultadoTipoEstres = "Uste presenta estres grave";
       this.test.ResultadoDescripcion = "Según este breve test, presentas estrés grave en el test burnout sugiere que tu nivel de estrés laboral está afectando gravemente a tu bienestar emocional. El síndrome del burnout puede tener consecuencias muy negativas en nuestro cuerpo y mente, por lo tanto, es importante que acudas a un especialista para que te sugiera nuevas dinámicas y así mejorar tu ambiente laboral."
     }
+  }*/
+
+  salario: number = 0;
+  tipoDeRiesgo:String = "";
+  tipoDeTrabajador:String = "Trabajador dependiente."
+
+  cambiarTipoEmpleado(valor:any){
+    this.tipoDeTrabajador = valor
+  }
+
+  capturarSalario(){
+    let aux=((this.aportes.Salario*40)/100);
+    if( aux< this.SMV){
+      this.aportes.IBC= this.SMV;
+    }else if(aux>=this.SMV){
+      this.aportes.IBC=aux;
+    }
+    this.aportes.TipoTrabajador = this.tipoDeTrabajador
+    this.CapturarTipoRiesgo(this.tipoDeRiesgo);
+    this.CalcularAportes();
+  }
+
+
+
+  CapturarTipoRiesgo(val:any){
+    if(val=="I Riesgo mínimo"){
+      this.aportes.ValotRiesgo=0.522;
+    }else if(val=="II Riesgo bajo"){
+      this.aportes.ValotRiesgo=1.044;
+    }else if(val=="III Riesgo medio"){
+      this.aportes.ValotRiesgo=2.436;
+    }else if(val=="IV Riesgo alto"){
+      this.aportes.ValotRiesgo=4.350;
+    }else if(val=="V Riesgo máximo"){
+      this.aportes.ValotRiesgo=6.960;
+    }
+
+  }
+
+  CalcularAportes(){
+    debugger
+    if(this.aportes.TipoTrabajador=="Trabajador dependiente."){
+     this.aportes.AFPE =( (this.aportes.Salario*12)/100);
+     this.aportes.AFPT =( (this.aportes.Salario*4)/100);
+     this.aportes.EPSE=( (this.aportes.Salario*8.5)/100);
+     this.aportes.EPST=( (this.aportes.Salario*4)/100);
+     this.aportes.ARLE=( (this.aportes.Salario*this.aportes.ValotRiesgo)/100);
+     this.aportes.ARLT=0;
+    
+    }else if(this.aportes.TipoTrabajador=="Trabajador independiente."){
+      this.aportes.AFPE = 0;
+      this.aportes.AFPT =( (this.aportes.IBC*16)/100);
+      this.aportes.EPSE=0;
+      this.aportes.EPST=( (this.aportes.IBC*12.5)/100);
+      if((this.aportes.ValotRiesgo==0.522 ) || (this.aportes.ValotRiesgo ==1.044) || (this.aportes.ValotRiesgo==2.436)){
+        this.aportes.ARLE=0;
+        this.aportes.ARLT=( (this.aportes.IBC*this.aportes.ValotRiesgo)/100);
+      }else{
+        this.aportes.ARLE=( (this.aportes.IBC*this.aportes.ValotRiesgo)/100);
+        this.aportes.ARLT=0;
+      }
+    }
+    this.aportes.SubtotalEmpleador=(this.aportes.AFPE+this.aportes.EPSE+this.aportes.ARLE);
+    this.aportes.SubtotalTrabajador=( this.aportes.AFPT+ this.aportes.EPST+this.aportes.ARLT);
+    this.aportes.Total=(this.aportes.SubtotalEmpleador+this.aportes.SubtotalTrabajador);
+    //alert("Total: "+this.aportes.Total+ " SubTotalEmpleador: "+ this.aportes.SubtotalEmpleador+ " subTotalTrabajador: " + this.aportes.SubtotalTrabajador)
   }
 }
---
+++ src/app/model/aportes.ts
@@ -0,0 +1,16 @@
+export class Aportes {
+    Salario:number=0;
+    IBC:number=0;
+     AFPE:number=0;
+     AFPT:number=0;
+    EPSE:number=0;
+    EPST:number=0;
+    ARLE:number=0;
+    ARLT:number=0;
+    ValotRiesgo:number=0;
+    SubtotalEmpleador=0;
+    SubtotalTrabajador=0;
+    Total:number=0;
+    TipoTrabajador:String=""
+
+}
